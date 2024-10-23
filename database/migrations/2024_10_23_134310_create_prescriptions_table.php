d61d0f15a30e411857087e5a725f6027fb4563e3
riguel8 <s.diaz.ruelmiguel@cmu.edu.ph>
2024-10-23 21:45:11 +0800

    client side

--- database/migrations/2024_10_23_134310_create_prescriptions_table.php
+++ database/migrations/2024_10_23_134310_create_prescriptions_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
                 $table->id('PrescriptionID');
                 $table->foreignId('PatientID')->references('patientID')->on('patients')->onDelete('cascade');
                 $table->foreignId('DoctorID')->nullable();
-                $table->date('PrescriptionDate')->nullable();
+                $table->enum('Lens', ['SINGLE VISION', 'DOUBLE VISION', 'PROGRESSIVE', 'NEAR VISION'])->nullable();
+                $table->string('Frame', 255)->nullable();
+                $table->decimal('Price', 10, 2)->nullable();
+                $table->enum('Prescription', ['(OD) Right Eye', '(OS) Left Eye', '(OU) Both Eyes'])->nullable();
+                $table->date('PrescriptionDate')->default(now());
                 $table->text('PrescriptionDetails')->nullable();
                 $table->timestamps();
             });
