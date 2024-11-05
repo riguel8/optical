c005a1c2e338bbdae7587330fabcf05f03c5809e
riguel8 <s.diaz.ruelmiguel@cmu.edu.ph>
2024-11-05 11:04:14 +0800

    staff side

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,10 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\OphthalController;
-
-use App\Http\Controllers\Admin\EyewearController as AdminEyewearController;
-use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-
-
-use App\Http\Controllers\Staff\EyewearController as StaffEyewearController;
-use App\Http\Controllers\Staff\AppointmentController as StaffAppointmentController;
-use App\Http\Controllers\Staff\DashboardController as StaffDashboardController;
-
+// use App\Http\Controllers\Admin\AppointmentController;
+use App\Http\Controllers\Staff\EyewearController;
+use App\Http\Controllers\Staff\AppointmentController;
+use App\Http\Controllers\Staff\DashboardController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
@@ -23,30 +18,17 @@ Route::get('/', [PagesController::class, 'index'])->name('landing');
 
 // Admin Modules
 Route::middleware(['auth', 'verified', \App\Http\Middleware\UserTypeMiddleware::class . ':admin'])->group(function () {
-    
-    // Dashboard Module
-    // Route::get('/admin/dashboard', [StaffDashboardController::class, 'index'])->name('staff.dashboard');
-    Route::get('/admin/dashboard/get_appointments', [AdminDashboardController::class, 'getAppointments']);
-    Route::get('/admin/dashboard/get_appointment_details', [AdminDashboardController::class, 'getAppointmentDetails']);
-
+ 
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('/admin/patients', [AdminController::class, 'patients'])->name('admin.patients');
     Route::get('/admin/appointments', [AdminController::class, 'appointments'])->name('admin.appointments');
+    Route::get('/admin/eyewears', [AdminController::class, 'eyewears'])->name('admin.eyewears');
     Route::post('/admin/appointments/storeEyewear', [AdminController::class, 'storeEyewear'])->name('admin.storeEyewear');
     Route::post('/admin/appointments/storeAppointment', [AdminController::class, 'storeAppointment'])->name('admin.storeAppointment');
     
     Route::get('/admin/appointments/{id}', [AdminController::class, 'viewAppointment']);
     Route::get('/admin/appointments/edit/{id}', [AdminController::class, 'editAppointment']);
     Route::put('/admin/appointments/update/{id}', [AdminController::class, 'updateAppointment'])->name('admin.updateAppointment');
-
-    // Eyewear CRUD
-    Route::get('/admin/eyewears', [AdminEyewearController::class, 'index'])->name('admin.eyewears');
-    Route::post('/admin/eyewears', [AdminEyewearController::class, 'store'])->name('admin.eyewears.store');
-    Route::get('/admin/eyewears/view-details/{id}', [AdminEyewearController::class, 'view'])->name('admin.view-details');
-    Route::get('/admin/eyewears/edit/{id}', [AdminEyewearController::class, 'edit']);
-    Route::put('/admin/eyewears/update/{id}', [AdminEyewearController::class, 'update'])->name('admin.eyewears.update');
-    Route::delete('/admin/eyewears/{id}', [AdminEyewearController::class, 'delete'])->name('admin.eyewears.delete');
-
 });
  // Client Modules
 Route::middleware(['auth', 'verified', \App\Http\Middleware\UserTypeMiddleware::class . ':client'])->group(function () {
@@ -72,30 +54,29 @@ Route::middleware(['auth', 'verified', \App\Http\Middleware\UserTypeMiddleware::
 
 // Staff Modules
 Route::middleware(['auth', 'verified', \App\Http\Middleware\UserTypeMiddleware::class . ':staff'])->group(function () {
-    
-    // Dashboard Module
-    Route::get('/staff/dashboard', [StaffDashboardController::class, 'index'])->name('staff.dashboard');
-    Route::get('/staff/dashboard/get_appointments', [StaffDashboardController::class, 'getAppointments']);
-    Route::get('/staff/dashboard/get_appointment_details', [StaffDashboardController::class, 'getAppointmentDetails']);
+   
+    Route::get('/staff/dashboard', [DashboardController::class, 'index'])->name('staff.dashboard');
+    Route::get('/staff/dashboard/get_appointments', [DashboardController::class, 'getAppointments']);
+    Route::get('/staff/dashboard/get_appointment_details', [DashboardController::class, 'getAppointmentDetails']);
 
-    // Patient Module
     Route::get('/staff/patients', [StaffController::class, 'patients'])->name('staff.patients');
 
+
     // Appointment CRUD
-    Route::get('/staff/appointments', [StaffAppointmentController::class, 'index'])->name('staff.appointments');
-    Route::post('/staff/appointments', [StaffAppointmentController::class, 'store'])->name('staff.appointments.store');
-    Route::get('/staff/appointments/{id}', [StaffAppointmentController::class, 'view']);
-    Route::get('/staff/appointments/edit/{id}', [StaffAppointmentController::class, 'edit']);
-    Route::put('/staff/appointments/update/{id}', [StaffAppointmentController::class, 'update'])->name('staff.appointments.update');
-    Route::delete('/staff/appointments/{id}', [StaffAppointmentController::class, 'delete'])->name('staff.appointments.delete');
+    Route::get('/staff/appointments', [AppointmentController::class, 'index'])->name('staff.appointments');
+    Route::post('/staff/appointments', [AppointmentController::class, 'store'])->name('staff.appointments.store');
+    Route::get('/staff/appointments/{id}', [AppointmentController::class, 'view']);
+    Route::get('/staff/appointments/edit/{id}', [AppointmentController::class, 'edit']);
+    Route::put('/staff/appointments/update/{id}', [AppointmentController::class, 'update'])->name('staff.appointments.update');
+    Route::delete('/staff/appointments/{id}', [AppointmentController::class, 'delete'])->name('staff.appointments.delete');
 
     // Eyewear CRUD
-    Route::get('/staff/eyewears', [StaffEyewearController::class, 'index'])->name('staff.eyewears');
-    Route::post('/staff/eyewears', [StaffEyewearController::class, 'store'])->name('staff.eyewears.store');
-    Route::get('/staff/eyewears/view-details/{id}', [StaffEyewearController::class, 'view'])->name('staff.view-details');
-    Route::get('/staff/eyewears/edit/{id}', [StaffEyewearController::class, 'edit']);
-    Route::put('/staff/eyewears/update/{id}', [StaffEyewearController::class, 'update'])->name('staff.eyewears.update');
-    Route::delete('/staff/eyewears/{id}', [StaffEyewearController::class, 'delete'])->name('staff.eyewears.delete');
+    Route::get('/staff/eyewears', [EyewearController::class, 'index'])->name('staff.eyewears');
+    Route::post('/staff/eyewears', [EyewearController::class, 'store'])->name('staff.eyewears.store');
+    Route::get('/staff/eyewears/view-details/{id}', [EyewearController::class, 'view'])->name('staff.view-details');
+    Route::get('/staff/eyewears/edit/{id}', [EyewearController::class, 'edit']);
+    Route::put('/staff/eyewears/update/{id}', [EyewearController::class, 'update'])->name('staff.eyewears.update');
+    Route::delete('/staff/eyewears/{id}', [EyewearController::class, 'delete'])->name('staff.eyewears.delete');
 });
 
 // 404 Override
